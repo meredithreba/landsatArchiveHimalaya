ef90aecb9f02a1c419a38fc68b71adce93f83161
meredithreba <meredith.reba@yale.edu>
2018-02-24 17:00:28 -0500

    Batch file for listing sorted Landsat images on HPC.

---
+++ imageCSV.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=meredith.reba@yale.edu
+#SBATCH --job-name=imageCSV
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=day
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=5G
+#SBATCH -o yatsm_%A_%a.out
+
+# running code
+# load module
+# module load Langs/Python/2.7.10
+ 
+# using my anaconda environment
+source activate yatsm
+python /home/fas/seto/mra37/scripts/yatsm_listImagesCSV_Sort.py
