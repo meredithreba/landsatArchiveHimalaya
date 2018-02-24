5c857ea5920d70060d5e5eb9c8629e4d2a408e10
meredithreba <meredith.reba@yale.edu>
2018-02-24 16:49:29 -0500

    Batch file for running yatsm line on HPC at Yale.

---
+++ T4_yatsm_line.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --array=1-300
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=meredith.reba@yale.edu
+#SBATCH --job-name=YatsmLine
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=day,scavenge
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=5G
+#SBATCH -o yatsmline_%A_%a.out
+
+# running code
+# using my anaconda environment
+source activate yatsm
+
+yatsm -v line /home/fas/seto/mra37/scripts/T3_image.yaml "${SLURM_ARRAY_TASK_ID}" 300
+
+
+
