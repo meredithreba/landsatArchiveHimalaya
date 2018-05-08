abc374e467fe7b9feecfbbed1f201080f58aa0ff
bhchai <31667628+bhchai@users.noreply.github.com>
2018-05-08 16:53:52 -0400

    Add files via upload

---
+++ topcor_141041_SE_parTest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=baohui.chai@yale.edu
+#SBATCH --job-name=141041SEParTest
+#SBATCH --ntasks-per-node=6
+#SBATCH --nodes=2
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=day
+#SBATCH --time=24:00:00
+#SBATCH -o Outfiles/141041SEParTest_%A_%a.out
+
+module load Apps/R
+module load Rpkgs/RASTER
+module load Rpkgs/RGDAL
+module load Rpkgs/DOSNOW
+module load Rpkgs/FOREACH
+module load Rpkgs/RGEOS
+module load Rpkgs/XML
+module load Rpkgs/RMPI/0.6.3
+module load MPI/OpenMPI
+
+# running code
+
+export OMPI_MCA_mtl=^psm
+mpirun --oversubscribe -n 1 R --slave -f topcor_141041_SE_parTest.R
