c4911f69517da8dbfef5b0a258acf91266028e91
bhchai <31667628+bhchai@users.noreply.github.com>
2018-09-20 14:32:36 -0400

    Create TopCor_JpbArrywithR.sh

---
+++ TopCor_JpbArrywithR.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=baohui.chai@yale.edu
+#SBATCH --job-name=141041clipSEJobArryTest
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=day
+#SBATCH --time=24:00:00
+#SBATCH --array=1-500
+#SBATCH -o Outfiles/141041clipSEJobArryTest_%A_%a.out
+
+module load Apps/R/3.3.2-generic
+module load Rpkgs/RASTER
+module load Rpkgs/RGDAL
+module load Rpkgs/RGEOS
+module load Rpkgs/XML
+
+file=$(ls /home/fas/seto/bc643/scratch60/grace1/141041clipped/L?0?141041??????????T?_clipped/L?0?141041??????????T?_clipped.tif | head -n $SLURM_ARRAY_TASK_ID | tail -1 )
+export  file
+R --vanilla --no-readline -q  << 'EOF'
+
+##############################################################
+file <- Sys.getenv(c('file'))
+library(rgdal)
+library(raster)
+library(RStoolbox)
+
+###############################################################
+if (file.exists('/home/fas/seto/bc643/scratch60/grace1/141041clipped_topCor_SE_5bandsAndQA_0920test')==FALSE) {
+	dir.create('/home/fas/seto/bc643/scratch60/grace1/141041clipped_topCor_SE_5bandsAndQA_0920test', showWarnings = TRUE)
+}
+
+print(file)
+###############################################################
+#e.g., /home/fas/seto/bc643/scratch60/grace1/141041clipped/LC081410412013042401T1_clipped
+wkdir <- paste0("/home/fas/seto/bc643/scratch60/grace1/141041clipped/",strsplit(file[1],'[/]')[[1]][9],sep='')
+print(wkdir)
+setwd(wkdir)
+files1 <- list.files()
+tifNameFirstPart <- strsplit(files1[1],'[.]')[[1]][1] #e.g., LC081410412013042401T1
+outfile <- paste(tifNameFirstPart,"topCor_SE",sep='_')
+Oristack <- grep('tif', files1, value=TRUE)
+print(Oristack)
+#Stack bands 2,3,4,5,7
+
+###############################################################
+fullpath <- paste0("/home/fas/seto/bc643/scratch60/grace1/141041clipped_topCor_SE_5bandsAndQA_0920test/",outfile,sep='')
+fullpathTif_se <- paste0(fullpath,"/",outfile,sep='')
+if (file.exists(fullpath)==FALSE) {
+	dir.create(fullpath, showWarnings = TRUE)
+	print(fullpathTif_se)
+	tif <- stack(Oristack, bands = as.integer(c(2,3,4,5,6)))
+
+###############################################################
+	wholesrtm <- raster("/home/fas/seto/bc643/scratch60/grace1/141041_SRTM_mosaic_tif.tif")
+
+	#crop, reproject and resample the SRTM
+	wholesrtm_30_utm <- projectRaster(wholesrtm, tif, method="bilinear", alignOnly=FALSE, over=FALSE)
+	mysrtm <- wholesrtm_30_utm
+	#pixel qa band as mask
+	m = raster(Oristack,band=8)
+	#mark the values of water&cloud-free clear pixels as 2
+	m[m==66|m==130|m==322|m==386|m==400|m==834|m==898|m==1346] <- 2   
+	if(sum(m[]!=2)==ncell(Oristack)){
+		file.remove(fullpath)
+		next
+	}
+	else{
+		#change the values of water and cloud pixels into NA
+		m[m!=2]<-NA    
+		#mask out water and cloud
+		tif_masked <- mask(tif,m)    
+
+		tif_se <- topCor(tif_masked, dem = mysrtm, metaData = grep('txt', files1, value=TRUE), method = "stat")
+		
+		tif_se <- addLayer(tif_se, m)
+		writeRaster(tif_se,fullpathTif_se,format="GTiff",datatype='INT2S')
+		print(fullpathTif_se)
+	}
+	#copy MTL file to TopCor folder
+	file.copy(from=grep('txt', files1, value=TRUE), to=fullpath, overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
+}
+EOF
