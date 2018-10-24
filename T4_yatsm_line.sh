03a84bb179a5ee28914de1177c304fc5a7152644
bhchai <31667628+bhchai@users.noreply.github.com>
2018-10-24 17:22:06 -0400

    Update T4_yatsm_line.sh

--- T4_yatsm_line.sh
+++ T4_yatsm_line.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --array=1-300
+#SBATCH --array=1-600
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=meredith.reba@yale.edu
 #SBATCH --job-name=YatsmLine
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --partition=week
-#SBATCH --time=120:00:00
+#SBATCH --partition=day
+#SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=5G
 #SBATCH -o yatsmline_%A_%a.out
 
@@ -15,7 +15,7 @@
 # using my anaconda environment
 source activate yatsm
 
-yatsm -v line /home/fas/seto/mra37/scripts/T3_image.yaml "${SLURM_ARRAY_TASK_ID}" 300
+yatsm -v line /home/fas/seto/mra37/scripts/T3_image.yaml "${SLURM_ARRAY_TASK_ID}" 600
 
 
 
