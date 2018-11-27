1f18753d748aebb030cdf094832a7e1452dbd8fe
lixinyi328 <45050352+lixinyi328@users.noreply.github.com>
2018-11-26 22:00:47 -0500

    Run trajectory_full_HPC_slice.m on HPC

---
+++ trajectory.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=xinyi.li@yale.edu
+#SBATCH --job-name=trajectory
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=day,pi_seto
+#SBATCH --time=24:00:00
+##SBATCH --mem-per-cpu=2048mb
+#SBATCH -o Outfiles/trajectory.out
+
+# running code
+module load Apps/Matlab/R2014a
+
+matlab -nodisplay < trajectory_full_HPC_slice.m
