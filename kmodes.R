3eaacff0d18f18c0004cc30006329ecd332235cc
lixinyi328 <45050352+lixinyi328@users.noreply.github.com>
2018-12-11 22:28:37 -0500

    Run kmodes to cluster trajectories

---
+++ kmodes.R
@@ -0,0 +1,8 @@
+library(klaR)
+A<- read.delim("/home/fas/seto/xl533/scratch60/grace6/urban_only_traj_round.txt",header = FALSE,sep = "", dec = ".")
+nc=ncol(A)
+nr=nrow(A)
+B=A[2:nr,3:nc]
+print("DONE READ IN")
+(cl <- kmodes(B, 10))
+write.table(cl$cluster, file = "/home/fas/seto/xl533/scratch60/grace6/kmodes.txt",quote=FALSE,row.names=FALSE,col.names=FALSE)
