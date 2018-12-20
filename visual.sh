7911ede941cd3095a940b5c26c639e9d1eb29ca3
lixinyi328 <45050352+lixinyi328@users.noreply.github.com>
2018-12-20 14:19:40 +0800

    Update visual.sh

--- visual.sh
+++ visual.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=day,pi_seto
 #SBATCH --time=24:00:00
-##SBATCH --mem-per-cpu=2048mb
+#SBATCH --mem-per-cpu=2048mb
 #SBATCH -o Outfiles/visual.out
 
 # running code
@@ -20,6 +20,6 @@ matlab -nodisplay < urban_only_HPC.m
 
 #the sequence of visualization is:
 #1.matrix.sh (matrix.py)
-#2.visual.sh (trajectory_full_HPC_medium.m + urban_only_HPC.m)
+#2.visual.sh (urban_medium_HPC.m)
 #3.kmodes.sh (kmodes.R)
 #4.visual.sh (traj_visualization_HPC_mode.m)
