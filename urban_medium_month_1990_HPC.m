e5eb7d1d3b84c0da857daab217d67f135bad7787
lixinyi328 <45050352+lixinyi328@users.noreply.github.com>
2018-12-26 10:57:43 +0800

    Update urban_medium_month_1990_HPC.m

--- urban_medium_month_1990_HPC.m
+++ urban_medium_month_1990_HPC.m
@@ -1,5 +1,5 @@
 clear;clc;
-array=load('/home/fas/seto/xl533/scratch60/grace2/out.txt');
+array=load('/home/fas/seto/xl533/scratch60/grace1/out.txt');
 [r,c]=size(array);
 pixelXmax=max(array(:,1));
 pixelYmax=max(array(:,2));
@@ -10,9 +10,6 @@ vector_len=maxx-minn;
 cut_start=726467; %1990-01-01 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 cut_end=736693; %2017-12-31
 desired_len=round((cut_end-cut_start)/30.4375);
-fid=fopen('/home/fas/seto/xl533/scratch60/grace2/traj_urban_medium_front_end_month_1990.txt','w');
-fprintf(fid,'%d ',minn,maxx);
-fprintf(fid,'\n ');
 
 urban_index=nan(1,2);
 for k=1:r                  %go through all items in matrix.txt
@@ -32,16 +29,25 @@ end
 urban_index=unique(urban_index,'rows');
 [r_u,c_u]=size(urban_index);
 urban_index(r_u,:)=[]; %delete nan rows
+fid=fopen('/home/fas/seto/xl533/scratch60/grace1/urban_index_new.txt','w');
+for i=1:r_u-1
+    fprintf(fid,'%d ',urban_index(i,:));
+    fprintf(fid,'\n ');
+end
+fclose(fid);
 
 traj=zeros(r_u,vector_len+2);
 traj_month=zeros(r_u-1,desired_len+2);
 traj_day=zeros(1,cut_end-cut_start);
+fid=fopen('/home/fas/seto/xl533/scratch60/grace1/traj_urban_medium_front_end_month_1990.txt','w');
+fprintf(fid,'%d ',minn,maxx);
+fprintf(fid,'\n ');
 
 for i=1:r_u-1
-    traj(i,1)=urban_index(i,1);
-    traj(i,2)=urban_index(i,2);  %write in pixelX + pixelY
     for k=1:r
-        if array(k,1)==traj(i,1) && array(k,2)==traj(i,2) 
+        traj(i,1)=urban_index(i,1);  %update:12/26/2018
+        traj(i,2)=urban_index(i,2);  %write in pixelX + pixelY
+        if array(k,1)==traj(i,1) && array(k,2)==traj(i,2)
             pixelX=array(k,1);
             pixelY=array(k,2);
             startDate=array(k,3);
@@ -68,12 +74,20 @@ for k=1:r_u-1
             break
         end
     end
+    if traj(k,3)~=0        %update:12/26/2018
+        breakx0=3;
+    end
     
     for i=3:vector_len+1
         if traj(k,i)~=0 && traj(k,i+1)==0          %find the very last ending date, from 1/2/3/4/5 to 0
             breakxmax=i;
         end
     end
+    
+    if traj(k,vector_len+2)~=0   %update:12/26/2018
+        breakxmax=vector_len+2;
+    end
+        
         
     for i=4:breakxmax-1       %this part is to write in data between models' break. e.g:330055->333555
         if traj(k,i)~=0 && traj(k,i+1)==0  %find the date before the change
@@ -103,7 +117,7 @@ for k=1:r_u-1
         traj_day(1,m)=traj(k,m+cut_start-minn);
     end
     for m=1:desired_len
-        traj_month(k,m+2)=traj_day(1,round(m*30.4375)-1); %turn into a month level, 1990-01-01 to 2017-12-31
+        traj_month(k,m+2)=traj_day(1,round(m*30.4375)-1); %average day: 365.25/12=30.4375
     end
     traj_month(k,1)=traj(k,1); 
     traj_month(k,2)=traj(k,2);
@@ -114,4 +128,4 @@ end
 
 fclose(fid);
 [r_m,c_m]=size(traj_month);
-c_m-2   %the output number should be 336
+c_m-2
