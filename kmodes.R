25915d7e47455d6a468b63f2fd79d8bec049b816
bhchai <31667628+bhchai@users.noreply.github.com>
2018-12-26 17:51:46 -0500

    Update kmodes.R

--- kmodes.R
+++ kmodes.R
@@ -4,5 +4,9 @@ nc=ncol(A)
 nr=nrow(A)
 B=A[2:nr,3:nc]
 print("DONE READ IN")
+######################Change cluster number accordingly
 (cl <- kmodes(B, 10))
 write.table(cl$cluster, file = "/home/fas/seto/xl533/scratch60/grace6/kmodes.txt",quote=FALSE,row.names=FALSE,col.names=FALSE)
+######################Change cluster number (denominator) accordingly
+sum(cl$withindiff/cl$size))/10
+
