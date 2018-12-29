e2a960c1d5474ee2a4b131ff2d867084611c46f8
lixinyi328 <45050352+lixinyi328@users.noreply.github.com>
2018-12-29 22:42:05 +0800

    Update hist_timing_5class_144040.m

--- hist_timing_5class_144040.m
+++ hist_timing_5class_144040.m
@@ -3,9 +3,9 @@ array=load('/home/fas/seto/xl533/scratch60/grace1/out_within.txt');
 
 [r,c]=size(array);
 
-breaktime_urban_from=zeros(5,28);
+breaktime_urban_from=zeros(5,28); %5 categories, 28 years
 breaktime_urban_to=zeros(5,28);
-for k=1:r                  %go through all items in matrix.txt
+for k=1:r                  %go through all items in out_within.txt
     pixelX=array(k,1);
     pixelY=array(k,2);
     startDate=array(k,3);
@@ -14,7 +14,7 @@ for k=1:r                  %go through all items in matrix.txt
     classType=array(k,6);
     
     if whetherBreak~=0 && classType==5      %find the pixels that change from urban
-        if endDate>726467 && endDate<736693 
+        if endDate>726467 && endDate<736693  %change date should between 1990-01-01 and 2017-12-31
         if array(k+1,6)==1 endDate_year=round(endDate/365); breaktime_urban_from(1,endDate_year-1990)=breaktime_urban_from(1,endDate_year-1990)+1; end
         if array(k+1,6)==2 endDate_year=round(endDate/365); breaktime_urban_from(2,endDate_year-1990)=breaktime_urban_from(2,endDate_year-1990)+1; end
         if array(k+1,6)==3 endDate_year=round(endDate/365); breaktime_urban_from(3,endDate_year-1990)=breaktime_urban_from(3,endDate_year-1990)+1; end
@@ -27,7 +27,7 @@ for k=1:r                  %go through all items in matrix.txt
     if k~=1 && classType==5 && pixelX==array(k-1,1) && pixelY==array(k-1,2)  %find the pixels that change into urban
         last_time=array(k-1,4);
         if last_time>726467 && last_time<736693 
-            if array(k-1,6)==1 last_time_year=round(last_time/365); breaktime_urban_to(1,last_time_year-1990)=breaktime_urban_to(1,last_time_year-1990)+1; end
+            if array(k-1,6)==1 last_time_year=round(last_time/365); breaktime_urban_to(1,last_time_year-1990)=breaktime_urban_to(1,last_time_year-1990)+1; end %count in
             if array(k-1,6)==2 last_time_year=round(last_time/365); breaktime_urban_to(2,last_time_year-1990)=breaktime_urban_to(2,last_time_year-1990)+1; end
             if array(k-1,6)==3 last_time_year=round(last_time/365); breaktime_urban_to(3,last_time_year-1990)=breaktime_urban_to(3,last_time_year-1990)+1; end
             if array(k-1,6)==4 last_time_year=round(last_time/365); breaktime_urban_to(4,last_time_year-1990)=breaktime_urban_to(4,last_time_year-1990)+1; end
@@ -36,7 +36,7 @@ for k=1:r                  %go through all items in matrix.txt
     end
 end
 
-fid=fopen('/home/fas/seto/xl533/scratch60/grace1/within_from_to_time_144040.txt','w');
+fid=fopen('/home/fas/seto/xl533/scratch60/grace1/within_from_to_time_144040.txt','w'); %write outputs
 %fid=fopen('F:\Semester1\Himalaya\CCDC\Codes\matrix_mine\old codes\from_to_time_1xx0xx.txt','w');
 fprintf(fid,'%s','breaktime_urban_from:');
 fprintf(fid,'\n ');
