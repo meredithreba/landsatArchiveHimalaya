35814fde6abdf73662aa96441567e11e82662daa
lixinyi328 <45050352+lixinyi328@users.noreply.github.com>
2018-12-30 12:03:57 +0800

    Update ismember_timing_144040.m

--- ismember_timing_144040.m
+++ ismember_timing_144040.m
@@ -2,17 +2,15 @@ clear;clc;
 out_ori=dlmread('/home/fas/seto/xl533/scratch60/grace1/out.txt');   %read in outputs from npz
 pixel_within=dlmread('/home/fas/seto/xl533/scratch60/grace1/144040_urbtraj_WithinShpnoOverlay_matlab.txt'); %read in urban only pixels (not overlapped)
 [r_o,c_o]=size(out_ori);
-[r_w,c_w]=size(pixel_within);
 fid=fopen('/home/fas/seto/xl533/scratch60/grace1/out_within.txt','w');  %output.txt
 
 
 for i=1:r_o
-    for j=1:r_w
-        if pixel_within(j,1)==out_ori(i,1) && pixel_within(j,2)==out_ori(i,2)  %ismember
+        index=[out_ori(i,1),out_ori(i,2)];
+        if ismember(index,pixel_within,'rows')==1 %ismember
             fprintf(fid,'%d ',out_ori(i,:)); %write results
             fprintf(fid,'\n ');
         end
-    end
 end
 
 fclose(fid);
