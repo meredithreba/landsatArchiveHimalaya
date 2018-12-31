0016fa5814e72c0afb6cd271e3ce148bf9a0d06d
lixinyi328 <45050352+lixinyi328@users.noreply.github.com>
2018-12-31 13:01:53 +0800

    Update kmodes.R

--- kmodes.R
+++ kmodes.R
@@ -2,7 +2,7 @@ library(klaR)
 A<- read.delim("/home/fas/seto/xl533/scratch60/grace6/urban_only_traj_round.txt",header = FALSE,sep = "", dec = ".")
 nc=ncol(A)
 nr=nrow(A)
-B=A[2:nr,3:nc]
+B=A[1:nr,3:nc]
 print("DONE READ IN")
 ######################Change cluster number accordingly
 (cl <- kmodes(B, 10))
