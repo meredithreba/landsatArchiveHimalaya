9e3e9941cbd407b73274fa8916ad43a1abd27878
bhchai <31667628+bhchai@users.noreply.github.com>
2019-02-27 13:50:07 +0800

    Mask water out of urban trajectories

--- mask_water_139041.m
+++ mask_water_139041.m
@@ -5,6 +5,7 @@ water=load('/home/fas/seto/bc643/scratch60/grace1/watercourses_txt/139041_waterc
 
 [rw,cw]=size(water);
 [ro,co]=size(out);
+out_new=out;
 
 pixelXout_min=min(out(:,1))
 pixelXout_max=max(out(:,1))
@@ -29,42 +30,42 @@ rw_end
 
 for i=1:ro
 	i
-	if i>ro
-		break
-	else
-		for j=rw_start:rw_end
-			% j
-			% water(j,1)<=out(i,1)
-			% water(j,2)<=out(i,2)
-			% disp('water(j,1)=');
-			% water(j,1)
-			% disp('water(j,2)=');
-			% water(j,2)
-			% disp('out(i,1)=');
-			% out(i,1)
-			% disp('out(i,2)=');
-			% out(i,2)
-			if water(j,1)<=out(i,1)
+	for j=rw_start:rw_end
+		% j
+		% water(j,1)<=out(i,1)
+		% water(j,2)<=out(i,2)
+		% disp('water(j,1)=');
+		% water(j,1)
+		% disp('water(j,2)=');
+		% water(j,2)
+		% disp('out(i,1)=');
+		% out(i,1)
+		% disp('out(i,2)=');
+		% out(i,2)
+		if water(j,1)<=out(i,1)
+			if water(j,2)<=out(i,2)
 				if isequal(ismember(water(j,:),out(i,:)),[1 1])
-					out(i,:)=[];
-					ro=ro-1
+					out(i,:)=0;
 					disp('deleted: water(j,1), water(j,2)=');
 					water(j,1)
 					water(j,2)
 					break
 				end
 			else
-				break
+				continue
 			end
+		else
+			break
 		end
 	end
 end
 
-[ro_new,co_new]=size(out);
+out_new=out(any(out,2),:);
+[ro_new,co_new]=size(out_new);
 
-fid=fopen('/home/fas/seto/bc643/scratch60/grace1/urbOnlyTraj_WithinShpNoOverlay/urbOnlyTraj_WithinShpNoOverlay_breaktime/urbOnlyTraj_WithinShpNoOverlay_breaktime_month_maskwater_139041_faster.txt','w');
+fid=fopen('/home/fas/seto/bc643/scratch60/grace1/urbOnlyTraj_WithinShpNoOverlay/urbOnlyTraj_WithinShpNoOverlay_breaktime/urbOnlyTraj_WithinShpNoOverlay_breaktime_month_maskwater_139041.txt','w');
 for k=1:ro_new
-    fprintf(fid,'%d ',out(k,:));  %write out trajectories
+    fprintf(fid,'%d ',out_new(k,:));  %write out trajectories
     fprintf(fid,'\n ');
 end
 
